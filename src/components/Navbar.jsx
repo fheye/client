dbaa648528cb39dccba94b3ba81a04e08b6c376a
Emir Soyturk <emirsytrk@gmail.com>
2024-11-16 09:45:36 +0700

    wallet connect and disconnect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,36 @@
 import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 
 export default function Navbar() {
+    const [walletAddress, setWalletAddress] = useState('');
+
+    async function onConnectWallet() {
+        const res = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    
+        if (res.length > 0) {
+            setWalletAddress(res[0]);
+            
+            window.ethereum.on('accountsChanged', function (accounts) {
+                setWalletAddress(accounts[0]);
+            }
+            );
+            
+            window.ethereum.on('chainChanged', function () {
+                window.location.reload();
+            }
+            );
+
+            window.ethereum.on('disconnect', function () {
+                window.location.reload();
+            }
+            );
+        }
+    }
+
+    async function onDisconnectWallet() {
+        setWalletAddress('');
+    }
+
     return (
         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -8,11 +38,21 @@ export default function Navbar() {
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">FhEye</span>
                 </Link>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Connect Wallet</button>
+                    {
+                        walletAddress &&
+                        <div className="flex items-center space-x-3 rtl:space-x-reverse">
+                            <span className="text-sm font-medium text-gray-900 dark:text-white">{walletAddress.slice(0, 6)}...{walletAddress.slice(-4)}</span>
+                            <button onClick={onDisconnectWallet} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Disconnect</button>
+                        </div>
+                    }
+                    {
+                        !walletAddress &&
+                        <button onClick={onConnectWallet} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Connect Wallet</button>
+                    }
                     <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                         </svg>
                     </button>
                 </div>
